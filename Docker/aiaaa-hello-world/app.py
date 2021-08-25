334cdaccbc105ef356dfbf0d9dd23615a9f6d100
Jose Gato Luis <jose.gato@atos.net>
2021-08-25 12:18:21 +0200

    docker basic doc

---
+++ Docker/aiaaa-hello-world/app.py
@@ -0,0 +1,51 @@
+#   Copyright  2021 Atos Spain SA. All rights reserved.
+ 
+#   This file is part of EASIER AI.
+ 
+#  This service free software: you can redistribute it and/or modify it under the terms of AGPL License
+ 
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT ANY WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+#   BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT,
+#   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#   WHETHER IN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+#   OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#   See  LICENSE file for full license information  in the project root.
+
+# Author: Jose Gato Luis <jose.gato@atos.net>
+
+
+from flask import Flask
+from flask import render_template
+import os
+import random
+
+app = Flask(__name__)
+
+easier_model_version = 0
+
+@app.route("/")
+def hello():
+    return render_template("index.html", message=message_from_k8s)
+
+@app.route("/predict")
+def predict():
+    elements = [("good","https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-01-256.png"),
+    ("bad","https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-32-256.png"), 
+    ("warm","https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-21-256.png"), 
+    ("hot","https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-01-256.png"), 
+    ("cold","https://cdn4.iconfinder.com/data/icons/free-line-christmas-icons/24/Snowflake-256.png")]
+
+    element = random.choice(elements)
+    print (element)
+    return  render_template("predict.html", prediction=element[0], img =element[1])
+
+if __name__ == "__main__":
+
+    port = int(os.environ.get("PORT", 5000))
+    message_from_k8s= os.environ.get('MESSAGE', 'It seems there are no messages')
+    app.run(debug=True,host='0.0.0.0',port=port)
+
+# sudo docker build -t jgato/rest-hello-world .
+# sudo docker push jgato/rest-hello-world
+# sudo docker run -it jgato/rest-hello-world  app.py
+#  sudo docker run -it --entrypoint /bin/bash jgato/rest-hello-world
